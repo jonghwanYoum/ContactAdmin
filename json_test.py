6a5e19c612265330e75c99756b60dc084d8e88ae
jonghwanYoum <jonghwan.youm@gmail.com>
2016-07-01 22:06:07 +0900

    test

---
+++ json_test.py
@@ -0,0 +1,8 @@
+import json
+
+python_data = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+
+to_json_data = json.dumps(python_data,separators=(',',':'))
+to_json_data = json.dump(python_data,indent=4)
+
+print(to_json_data)
